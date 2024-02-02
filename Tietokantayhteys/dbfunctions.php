b638e5574b32513272922c0a1b8c4a89f0f92a09
irensula <irinawebdev@gmail.com>
2024-02-02 18:26:18 +0200

    Add files via upload

--- Tietokantayhteys/dbfunctions.php
+++ Tietokantayhteys/dbfunctions.php
@@ -7,7 +7,7 @@
 function connect() {
     $servername = "irysul23.treok.io";
     $username = "irysul23_sasp";
-    $password = "W8MS0s1Vn_,M";
+    $password = "saspiDm18&86";
     //$port = 3306;
     $dbname = "irysul23_sasp";
 
@@ -15,10 +15,82 @@ function connect() {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        // echo "Connected successfully";
         return $conn;
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
         die();
     }
 }
+
+// find all games from the table 'test_games'
+function getAllGames() {
+    $pdo = connect();
+    $sql = "SELECT * FROM test_games";
+    $stm = $pdo->query($sql);
+    $games = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $games;
+}
+
+// find game by id
+function getGameByID($id) {
+    $pdo = connect();
+    $sql = "SELECT * FROM test_games WHERE gameID=?";
+    $stm = $pdo->prepare($sql);
+    $stm->execute([$id]);
+    $game = $stm->fetch(PDO::FETCH_ASSOC);
+    return $game;
+}
+// find games by company
+function getGameByCompany($company) {
+    $pdo = connect();
+    $sql = "SELECT * FROM test_games WHERE company=?";
+    $stm = $pdo->prepare($sql);
+    $stm->execute([$company]);
+    $game = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $game;
+}
+
+// create new game
+function insertNewGame($name, $company, $release) {
+    $pdo = connect();
+    $sql = "INSERT INTO test_games (`name`, `company`, `release`) VALUES (?,?,?)";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$name, $company, $release]);
+    return $ok;
+}
+
+// update game
+function updateGame($name, $company, $release, $id) {
+    $pdo = connect();
+    $sql = "UPDATE test_games SET `name`=?, `company`=?, `release`=? WHERE gameID=?";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$name, $company, $release, $id]);
+    return $ok;
+}
+
+// delete game
+function deleteGame($id) {
+    $pdo = connect();
+    $sql = "DELETE FROM test_games WHERE gameID=?";
+    $stm = $pdo->prepare($sql);
+    $ok = $stm->execute([$id]);
+    return $ok;
+}
+
+// find all games from the table 'test_games'
+if($_POST['Show All Games']) {
+    
+    $submit = htmlspecialchars($_POST['Show All Games']);
+    
+    if($submit) {
+        function getAllGamesButton() {
+            $pdo = connect();
+            $sql = "SELECT * FROM test_games";
+            $stm = $pdo->query($sql);
+            $games = $stm->fetchAll(PDO::FETCH_ASSOC);
+            return $games;
+        } 
+    }
+}
+?>
--- Tietokantayhteys/index.php
+++ Tietokantayhteys/index.php
@@ -1,4 +1,84 @@
 <?php
+
   require "./dbfunctions.php";
 
-  $pdo = connect(); 
+  // $pdo = connect(); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD</title>
+</head>
+<body>
+  
+  <h2>Show All Games</h2>
+  <!-- find all games from the table 'test_games' -->
+  <ul>
+    <?php
+      $games = getAllGames();
+      foreach($games as $game) {
+        echo "<li>" . $game["name"] . "</li>";
+      }
+    ?>
+  </ul>
+
+  <h2>Show Games By Company's Name</h2>
+  <!-- find games by company -->
+  <ul>
+    <?php
+      $games =  getGameByCompany('Electronic Arts');
+      foreach($games as $game) {
+        echo "<li>" . $game["name"] . "</li>";
+      }
+    ?>
+  </ul>
+  
+  <h2>Show Game By ID</h2>
+  <!-- find game by id -->
+  <?php 
+      $game = getGameByID('1');
+      echo $game['name'];  
+  ?>
+
+  <h2>Create New Game</h2>
+  <!-- create new game -->
+  <?php
+   // echo insertNewGame('World of Warcraft', 'Blizzard Entertainment', '2004'); 
+   // echo insertNewGame('The Lord of the Rings: Heroes of Middle-earth', 'Electronic Arts', '2023'); 
+  ?>
+  
+  <h2>Update Game</h2>
+  <!-- update game -->
+  <?php
+   //  echo updateGame('Need for Speed: Underground 2', 'Electronic Arts', '2004', '1');
+  ?>
+
+  <h2>Delete Game</h2>
+  <!-- delete game -->
+  <?php
+   // echo deleteGame('5');
+  ?>
+
+  <h2>Show All Games Button</h2>
+  <!-- show all games button -->
+  <form action='index.php' method='post'>
+  <input type='submit' name='submit' value='Show All Games'>
+  
+  <ul>
+    <?php
+      $games = getAllGamesButton();
+      foreach($games as $game) {
+        echo "<li>" . $game["name"] . "</li>";
+      }
+    ?>
+  </ul>
+  
+</body>
+</html>
+
+
