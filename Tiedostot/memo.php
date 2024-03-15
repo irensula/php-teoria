d8bb75be1b39971f068e7ac37056bd4d9db2e12b
irensula <irinawebdev@gmail.com>
2024-03-15 19:16:15 +0200

    Add files via upload

---
+++ Tiedostot/memo.php
@@ -0,0 +1,22 @@
+<?php
+          
+    $text_1 = "Text_1";
+    $text_2 = "Text_2";
+
+    $file = fopen("memo.txt", "w");
+    fwrite($file, $text_1);
+    fclose($file);
+
+    $file = fopen("memo.txt", "w");
+    fwrite($file, $text_2);
+    fclose($file);
+
+    $file = fopen("memo.txt", "a");
+    fwrite($file, $text_1);
+    fclose($file);
+
+    $file = fopen("memo.txt", "r") or die("Unable to open file!");
+    $readFile = fread($file, filesize("memo.txt"));
+    echo $readFile;
+    
+?>
