5d9416386760313b97b7f3b733987b255c542f14
irensula <irinawebdev@gmail.com>
2024-03-25 15:34:17 +0200

    Add files via upload

--- Filtteröinti/index.php
+++ Filtteröinti/index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <!-- css -->
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/main.css">
+
     <title>Document</title>
 </head>
 <body>
+    <div class="container">
+    <h1 class="title">Filtteröinti</h1>
     <form action="index.php" method="get">
         <label for="name">Nimi:</label>
         <input type="text" name="name" id="name">
@@ -16,6 +26,8 @@
         <input type="email" name="email" id="email">
         <button type="submit">Submit</button>
     </form>
+    </div>  
+    
 </body>
 </html>
 
@@ -40,7 +52,17 @@
         } else {
             echo("Year must be a number.<br>");
         }
+
+        $min = 1999;
+        $max = 2009;
+        if (filter_var($intYear, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+        echo("Year is not within the legal range.<br>");
+        } else {
+        echo("Year is within the legal range.<br>");
+        }
+
         echo "Nimi: " . $name . ",<br>Syntymävuosi: " . $intYear . ",<br>Sähköposti: " . $email . ".<br>";
     }
+
    
 ?>
